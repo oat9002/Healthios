2ff465e2a2999530704538d435216853a476645d
oato.o <oat9002@hotmail.com>
2017-06-23 01:32:52 +0700

    Updated register.js

--- pages/register.js
+++ pages/register.js
@@ -1,64 +1,56 @@
+import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import TextField from 'material-ui/TextField';
+import Head from 'next/head';
 
-const Register = () => (
-  <MuiThemeProvider>
-    <form action="#" method='POST'>
-      <h1>Register</h1>
-      <br/>
-      <TextField floatingLabelText="Firstname" name='firstname'/>
-      <br/>
-      <TextField floatingLabelText="Lastname" name='lastname'/>
-      <br/>
-      <TextField multiLine={true} name='address' floatingLabelText='Address' style={{textAlign: 'left'}}></TextField>
-      <br/>
-      <TextField floatingLabelText="Email" type='email' name='email'/>
-      <br/>
-      <input id='submit' type="submit" value='Submit'/>
-      <style jsx>{`
-        form {
-          position: fixed;
-          text-align: center;
-          top: 35%;
-          left: 45%;
-          transform: translate(-35%, -45%);
-          animation: fadein 1s;
-        }
-        @keyframes fadein {
-            from { opacity: 0; };
-            to   { opacity: 1; };
-        }
-        h1 {
-          font-family: Roboto Light;
-        }
-        #submit {
-          margin-top: 50px;
-          width: 100%;
-          height: 50px;
-          max-width: 200px;
-          max-high: 50px;
-          border-radius: 50px;
-          border: 2px solid;
-          background-color: white;
-          color: black;
-          font-size: 20px;
-          transition: 0.8s;
-          white-space: nowrap;
-        }
-        #submit:hover {
-          background-color: black;
-          color: white;
-          transition: 0.8s;
-        }
-        #submit:active, #submit:focus, #submit.active {
-          background-image: none;
-          outline: 0;
-          -webkit-box-shadow: none;
-                      box-shadow: none;
-        }
-        `}</style>
-    </form>
-  </MuiThemeProvider>
-);
-
-export default Register
+export default class Register extends React.Component {
+  render() {
+    return(
+      <MuiThemeProvider>
+        <div className='content'>
+          <Head>
+            <link href="https://fonts.googleapis.com/css?family=Kanit:300,400" rel="stylesheet"/>
+            <link href="/static/css/animate.css" rel="stylesheet" />
+          </Head>
+          <div>
+            <span className='emph'>ไม่พบข้อมูลผู้ใช้</span>
+            <br/>
+            <span>กรุณาเสียบบัตรประชาชนเพื่อลงทะเบียน</span>
+            <br/>
+            <img className='slideInUp animated infinite' src="/static/pics/id.png"/>
+          </div>
+          <style jsx>{`
+            div {
+              font-family: Kanit Light;
+              color: #393939;
+              animation: fadein 1s;
+            }
+            @keyframes fadein {
+                from { opacity: 0; };
+                to   { opacity: 1; };
+            }
+            img {
+              heigh: 20%;
+              width: 20%;
+            }
+            span {
+              font-size: 48px;
+            }
+            .emph {
+              font-size: 74px;
+              font-weight: bold;
+            }
+            `}</style>
+            <style jsx global>{`
+              .content {
+                text-align: center;
+                margin-top: 13%;
+              }
+              body {
+                background-color: #f7f7f7;
+              }
+            `}</style>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
