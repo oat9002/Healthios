54b300c902a5e4c506ac3d175b1dd93716c51b11
oato.o <oat9002@hotmail.com>
2017-06-22 17:27:41 +0700

    Updated login.js

--- pages/login.js
+++ pages/login.js
@@ -1,71 +1,90 @@
 import Router from 'next/router';
-
+import Head from 'next/head';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const Login = () => (
-  <div>
-    <p>Tap your finger to fingerprint scanner</p>
-    <img src='/static/pics/fingerprints.svg'/>
-    <br/>
-    <button id='submit' onClick={() => Router.push('/register')}>Register</button>
-    <style jsx>{`
-      form {
-        position: fixed;
-        text-align: center;
-        top: 35%;
-        left: 45%;
-        transform: translate(-35%, -45%);
-      }
-      h1 {
-        font-family: Roboto Light;
-      }
-      #submit {
-        margin-top: 50px;
-        width: 100%;
-        height: 50px;
-        max-width: 200px;
-        max-high: 50px;
-        border-radius: 50px;
-        border: 2px solid;
-        background-color: white;
-        color: black;
-        font-size: 20px;
-        transition: 0.8s;
-        white-space: nowrap;
-      }
-      #submit:hover {
-        background-color: black;
-        color: white;
-        transition: 0.8s;
-      }
-      #submit:active, #submit:focus, #submit.active {
-        background-image: none;
-        outline: 0;
-        -webkit-box-shadow: none;
-                    box-shadow: none;
-      }
-      div {
-        position: fixed;
-        text-align: center;
-        font-family: Roboto Light;
-        top: 35%;
-        left: 42%;
-        transform: translate(-35%, -42%);
-        animation: fadein 1s;
-      }
-      @keyframes fadein {
-          from { opacity: 0; };
-          to   { opacity: 1; };
-      }
-      p {
-        font-size: 48px;
-        color: #999999;
-      }
-      img {
-        heigh: 10%
-        width: 10%
-      }
-      `}</style>
-  </div>
+  <MuiThemeProvider>
+    <div className='content'>
+      <Head>
+        <link href="https://fonts.googleapis.com/css?family=Kanit:300,400" rel="stylesheet"/>
+        <link href="/static/css/animate.css" rel="stylesheet" />
+      </Head>
+      <div>
+        <span>แตะนิ้วบนเครื่องแสกนลายนิ้วมือ</span>
+        <br/>
+        <img src='/static/pics/fingerprints.svg'/>
+      </div>
+      <div>
+        <span>หรือ</span>
+      </div>
+      <div>
+        <span>เสียบบัตรประชาชน</span>
+        <br/>
+        <img className='slideInUp animated infinite' src="/static/pics/id.png"/>
+      </div>
+      <br/>
+      {/* <button id='submit' onClick={() => Router.push('/register')}>Register</button> */}
+      <style jsx>{`
+        #submit {
+          margin-top: 50px;
+          width: 100%;
+          height: 50px;
+          max-width: 200px;
+          max-high: 50px;
+          border-radius: 50px;
+          border: 2px solid;
+          background-color: white;
+          color: black;
+          font-size: 20px;
+          transition: 0.8s;
+          white-space: nowrap;
+        }
+        #submit:hover {
+          background-color: black;
+          color: white;
+          transition: 0.8s;
+        }
+        #submit:active, #submit:focus, #submit.active {
+          background-image: none;
+          outline: 0;
+          -webkit-box-shadow: none;
+                      box-shadow: none;
+        }
+        div {
+          font-family: Kanit Light;
+          color: #393939;
+          animation: fadein 1s;
+        }
+        @keyframes fadein {
+            from { opacity: 0; };
+            to   { opacity: 1; };
+        }
+        div {
+          text-align: center;
+          width: 50%;
+        }
+        img {
+          heigh: 40%
+          width: 40%
+        }
+        span {
+          font-size: 48px
+        }
+        `}</style>
+        <style jsx global>{`
+          .content {
+            display: flex:
+            justify-content: center;
+            align-items: center;
+            margin-left: 13%;
+            margin-top: 13%;
+          }
+          body {
+            background-color: #f7f7f7;
+          }
+        `}</style>
+    </div>
+  </MuiThemeProvider>
 )
 
 export default Login
