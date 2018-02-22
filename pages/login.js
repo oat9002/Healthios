3a78e86e8938513a15d35e22a522014fd149b513
oat9002 <oat9002@hotmail.com>
2018-02-22 23:39:55 +0700

    Fixed login view

--- pages/login.js
+++ pages/login.js
@@ -164,83 +164,81 @@ export default class Login extends React.Component {
               <link href="https://fonts.googleapis.com/css?family=Kanit:200,300&amp;subset=thai" rel="stylesheet" />
               <link href="/static/css/animate.css" rel="stylesheet" />
             </Head>
-          <div>
-            <span className='emph'>แตะ</span><span>นิ้วบนเครื่องแสกนลายนิ้วมือ</span>
+            <div>
+              <span className='emph'>แตะ</span><span>นิ้วบนเครื่องแสกนลายนิ้วมือ</span>
+              <br/>
+              <img src='/static/pics/fingerprints.svg'/>
+            </div>
+            <div>
+              <span>หรือ</span>
+            </div>
+            <div>
+              <span className='emph'>เสียบ</span><span>บัตรประชาชน</span>
+              <br/>
+              <img className='slideInUp animated infinite' src="/static/pics/id.png"/>
+            </div>
             <br/>
-            <img src='/static/pics/fingerprints.svg'/>
-          </div>
-          <div>
-            <span>หรือ</span>
-          </div>
-          <div>
-            <span className='emph'>เสียบ</span><span>บัตรประชาชน</span>
-            <br/>
-            <img className='slideInUp animated infinite' src="/static/pics/id.png"/>
-          </div>
-          <br/>
-          {/* <button id='submit' onClick={() => Router.push('/register')}>Register</button> */}
-          <style jsx>{`
-          // #submit {
-          //   margin-top: 50px;
-          //   width: 100%;
-          //   height: 50px;
-          //   max-width: 200px;
-          //   max-high: 50px;
-          //   border-radius: 50px;
-          //   border: 2px solid;
-          //   background-color: white;
-          //   color: black;
-          //   font-size: 20px;
-          //   transition: 0.8s;
-          //   white-space: nowrap;
-          // }
-          // #submit:hover {
-          //   background-color: black;
-          //   color: white;
-          //   transition: 0.8s;
-          // }
-          // #submit:active, #submit:focus, #submit.active {
-          //   background-image: none;
-          //   outline: 0;
-          //   -webkit-box-shadow: none;
-          //               box-shadow: none;
-          // }
-          div {
-            text-align: center;
-            width: 50%;
-          }
-          img {
-            heigh: 40%;
-            width: 40%;
-          }
-          span {
-            font-size: 48px;
-          }
-          .emph {
-            font-size: 72px;
-            font-weight: 300;
-          }
+            {/* <button id='submit' onClick={() => Router.push('/register')}>Register</button> */}
+            <style jsx>{`
+            // #submit {
+            //   margin-top: 50px;
+            //   width: 100%;
+            //   height: 50px;
+            //   max-width: 200px;
+            //   max-high: 50px;
+            //   border-radius: 50px;
+            //   border: 2px solid;
+            //   background-color: white;
+            //   color: black;
+            //   font-size: 20px;
+            //   transition: 0.8s;
+            //   white-space: nowrap;
+            // }
+            // #submit:hover {
+            //   background-color: black;
+            //   color: white;
+            //   transition: 0.8s;
+            // }
+            // #submit:active, #submit:focus, #submit.active {
+            //   background-image: none;
+            //   outline: 0;
+            //   -webkit-box-shadow: none;
+            //               box-shadow: none;
+            // }
+            div {
+              width: 100%
+              text-align: center;
+            }
+            img {
+              heigh: auto;
+              width: 20%;
+            }
+            span {
+              font-size: 4em;
+            }
+            .emph {
+              font-size: 5em;
+              font-weight: 300;
+            }
+            `}</style>
+          <style jsx global>{`
+            .content {
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+            }
+            body {
+              background-color: #f7f7f7;
+              font-family: Kanit;
+              color: #393939;
+              animation: fadein 1s;
+              font-weight: 200;
+            }
+            @keyframes fadein {
+                from { opacity: 0; };
+                to   { opacity: 1; };
+            }
           `}</style>
-        <style jsx global>{`
-          .content {
-            display: flex:
-            justify-content: center;
-            align-items: center;
-            margin-left: 13%;
-            margin-top: 13%;
-          }
-          body {
-            background-color: #f7f7f7;
-            font-family: Kanit;
-            color: #393939;
-            animation: fadein 1s;
-            font-weight: 200;
-          }
-          @keyframes fadein {
-              from { opacity: 0; };
-              to   { opacity: 1; };
-          }
-        `}</style>
         </div>
       )}
       </MuiThemeProvider>
