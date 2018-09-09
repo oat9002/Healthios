2c379fa8d97c0e3cb0a45f33d85f6682f4b97535
oat9002 <oat9002@hotmail.com>
2018-09-09 14:12:16 +0700

    Removed unused

--- pages/loginComplete.js
+++ pages/loginComplete.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import CompleteTemplate from '../components/completeTemplate';
 import Router from 'next/router';
-import PropTypes from 'prop-types';
-
-registerComplete.propTypes = {
-  url: PropTypes.object
-};
 
 export default class registerComplete extends React.Component {
 
   componentDidMount() {
     setTimeout(() => {
-      Router.push({ pathname: '/welcome', query: { first: this.props.url.query.first } });
+      Router.push({ pathname: '/welcome');
     }, 3000);  
   }
 
