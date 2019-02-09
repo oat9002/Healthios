76debc523d5cac6cb35f61a7465aec37eb866505
oat9002 <oat9002@hotmail.com>
2019-02-09 17:55:44 +0700

    Fix final

--- pages/final.js
+++ pages/final.js
@@ -9,6 +9,10 @@ class Final extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      isCardInsert: false
+    };
+
     this.pageTimeout = null;
     this.retryCheckDeattachCardTimeout = null;
   }
@@ -31,13 +35,16 @@ class Final extends React.Component {
 
     try {
       const resIsInsertCard = await axios.get(urlIsInsertCard);
-      if(resIsInsertCard === undefined || resIsInsertCard.data.status) {
+      if (resIsInsertCard === undefined || resIsInsertCard.data.status) {
+        this.setState({
+          isCardInsert: true
+        });
         throw new Error('Card is still inserted or validate failed.');
       }
 
       Router.replace('/');
     }
-    catch(err) {
+    catch (err) {
       Logging.sendLogMessage('Check deattach card failed', err);
       this.retryCheckDeattachCard();
     }
@@ -49,40 +56,47 @@ class Final extends React.Component {
 
   render() {
     return (
-      <div className='content'>
-        <Head>
-          <link href="https://fonts.googleapis.com/css?family=Kanit:200,300&amp;subset=thai" rel="stylesheet" />
-          <link href="/static/css/animate.css" rel="stylesheet" />
-        </Head>
-        <span>เสร็จสิ้นการทำงาน</span>
-        <br/>
-        <span className='sub'>กรุณาดึงบัตรประชาชนออก(ถ้ามี)</span>
-        <style jsx>{`
-          .sub {
-            font-size: 0.3em;
-          }
-          .content {
-            font-size: 7em;
-            text-align: center;
-            margin-top: 20%;
-          }
-          img {
-            vertical-align:middle;
-          }
-        `}</style>
-        <style jsx global>{`
-          body {
-            font-family: Kanit;
-            color: #393939;
-            background-color: #f7f7f7;
-            animation: fadein 1s;
-          }
-          @keyframes fadein {
-              from { opacity: 0; };
-              to   { opacity: 1; };
-          }
-        `}</style>
-      </div>
+      <React.Fragment>
+        {this.state.isCardInsert
+          ? (
+            <div className='content'>
+              <Head>
+                <link href="https://fonts.googleapis.com/css?family=Kanit:200,300&amp;subset=thai" rel="stylesheet" />
+                <link href="/static/css/animate.css" rel="stylesheet" />
+              </Head>
+              <span>เสร็จสิ้นการทำงาน</span>
+              <br />
+              <span className='sub'>กรุณาดึงบัตรประชาชนออก</span>
+              <style jsx>{`
+              .sub {
+                font-size: 0.3em;
+              }
+              .content {
+                font-size: 7em;
+                text-align: center;
+                margin-top: 20%;
+              }
+              img {
+                vertical-align:middle;
+              }
+            `}</style>
+              <style jsx global>{`
+              body {
+                font-family: Kanit;
+                color: #393939;
+                background-color: #f7f7f7;
+                animation: fadein 1s;
+              }
+              @keyframes fadein {
+                  from { opacity: 0; };
+                  to   { opacity: 1; };
+              }
+            `}</style>
+            </div>
+          )
+          : null}
+      </React.Fragment>
+
     );
   }
 }
