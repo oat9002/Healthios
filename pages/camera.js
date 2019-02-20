49dd1014b9492f120e35bb5eda71dc5234e30a30
oat9002 <oat9002@hotmail.com>
2019-02-20 23:08:34 +0700

    Initialize webcam

---
+++ pages/camera.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+
+class Camera extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  setRef = webcam => {
+    this.webcam = webcam;
+  };
+
+  capture = () => {
+    const imageSrc = this.webcam.getScreenshot();
+    console.log(imageSrc);
+  };
+
+  render() {
+    const videoConstraints = {
+      width: 1280,
+      height: 720,
+      facingMode: 'user'
+    };
+
+    return (
+      <div>
+        <Webcam
+          audio={false}
+          height={800}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={800}
+          videoConstraints={videoConstraints}
+        />
+        <button onClick={this.capture}>Capture photo</button>
+      </div>
+    );
+  }
+  
+}
+
+export default Camera;
